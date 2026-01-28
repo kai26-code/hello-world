0de6698ac3bd3535686b52bd298e9e9b41d540f2
kai26-code <kai.ostaszewski@student.fairfield.edu>
2026-01-28 14:45:20 -0500

    Custom hello

--- print('hello world').py
+++ print('hello world').py
@@ -1,1 +1,6 @@
-print('hello world')
+# this program prints hello, name and program greets them
+name = input("whats your name?").title()
+clean = " ".join(name.split())
+print(f"hello, {clean}") 
+ 
+
